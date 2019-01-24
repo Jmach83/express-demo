6eead6438ae78cc3e362150831082d7434d90735
ASOL\jma <jma@alpha-solutions.dk>
2019-01-24 15:42:46 +0100

    Handling posts request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1' },
     { id: 2, name: 'course2' },
@@ -17,11 +19,11 @@ app.get('/api/courses', (req, res) => {
 
 app.post('/api/courses', (req, res) => {
     const course = {
-        id: course.length +1,
+        id: courses.length +1,
         name: req.body.name
     };
-    course.push(course);
-    res.push(course);
+    courses.push(course);
+    res.send(course);
 })
 
 app.get('/api/courses/:id', (req, res) => {
